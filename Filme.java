8c5bf7104ccf5060a3440687ab515e9478c14e12
kaio c. vieira <136288418+shinitsukkaio@users.noreply.github.com>
2026-06-03 18:48:08 -0300

    Create Filme.java

---
+++ Filme.java
@@ -0,0 +1,11 @@
+class Filme {
+    String nome;
+    String categoria;
+    int duracao;
+    
+    public Filme(String nome, String categoria, int duracao) {
+        this.nome = nome;
+        this.categoria = categoria;
+        this.duracao = duracao;
+    }
+}
